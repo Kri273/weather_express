5a72d1fde56591243c88d01c00bc2b595a152b2e
Kristelle Tasane <kristelle.tasane@voco.ee>
2024-09-30 11:00:13 +0300

    Added API to project template file

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 const express = require('express')
 const app = express()
 const port = 3003
-
-
-
 const path = require('path')
+const fetch = require('node-fetch')
+
+
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+const key = '6e366b3ae34dc6bdd56794422668a907'
+let city = 'Tartu'
 
 app.get('/', (req, res) => {
-    res.render('index')
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+        .then((responce) => {
+            return responce.json()
+        })
+        .then((data) => {
+            let description = data.weather[0].description
+            let city = data.name
+            let temp = Math.round(parseFloat(data.main.temp)-273.15)
+            res.render('index', {
+                description: description,
+                city: city,
+                temp: temp
+            })
+        })
 })
 
 app.listen(port, () => {
     console.log(`http://localhost:${port}`)
-  })
+})
