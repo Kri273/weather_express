43229c43b2a75a9013f722c6d31ed22addfbe714
Kristelle Tasane <kristelle.tasane@voco.ee>
2024-09-30 11:35:06 +0300

    Created form request responce with API data

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const port = 3003
 const path = require('path')
 const fetch = require('node-fetch')
 
-
+// proceed form post method data
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
@@ -19,7 +21,25 @@ app.get('/', (req, res) => {
         .then((data) => {
             let description = data.weather[0].description
             let city = data.name
-            let temp = Math.round(parseFloat(data.main.temp)-273.15)
+            let temp = Math.round(parseFloat(data.main.temp) - 273.15)
+            res.render('index', {
+                description: description,
+                city: city,
+                temp: temp
+            })
+        })
+})
+
+app.post('/', function (req, res) {
+    let city = req.body.cityname
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+        .then((responce) => {
+            return responce.json()
+        })
+        .then((data) => {
+            let description = data.weather[0].description
+            let city = data.name
+            let temp = Math.round(parseFloat(data.main.temp) - 273.15)
             res.render('index', {
                 description: description,
                 city: city,
